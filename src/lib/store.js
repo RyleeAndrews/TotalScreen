091b96a821db5d5b5a542e321ad165513a7208ee
Rylee Andrews <ryleeandrews73@gmail.com>
2019-04-01 14:52:56 -0600

    trying to deploy heroku

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 
-import reducer from './combineReducers.js';
-import reporter from './middleware/reporter.js';
-import thunk from './middleware/thunk.js';
+import reducer from './combineReducer.js';
+import reporter from './reporter.js';
+import thunk from './thunk.js';
 
 export default () => createStore(reducer, applyMiddleware(thunk,reporter));
