85a2330cae52c9e394d0ea6db3382205be88feb7
Rylee Andrews <ryleeandrews73@gmail.com>
2019-04-01 14:37:35 -0600

    added base code working on all new website

---
+++ src/components/app.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import {Route} from 'react-router-dom';
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+
+
+        <main>
+          
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
